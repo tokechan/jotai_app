12fcb8b44c8434cbe180971b33321ecbb9eb371b
toketime <flatoke@gmail.com>
2025-02-13 14:33:37 +0900

    create base env

--- jotai-note-app/src/domain/note.ts
+++ jotai-note-app/src/domain/note.ts
@@ -2,7 +2,7 @@ export class Note {
     constructor(
         public id: string,
         public title: string,
-        public contente: string,
+        public content: string,
         public lastEditTime: number
     ) {}
 }
